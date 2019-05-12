db4adfd5edba9f81ea5cb2d532318f732fa32a11
ranggakusuma <ranggavskusuma@gmail.com>
2019-05-12 13:50:25 +0700

    add decode and encode functions

---
+++ constant.js
@@ -0,0 +1,10 @@
+const keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+const RESPONSE_MESSAGE = {
+  INVALID_BASE64: 'There were invalid base64 characters in the input text.',
+};
+
+module.exports = {
+  keyStr,
+  RESPONSE_MESSAGE,
+};
